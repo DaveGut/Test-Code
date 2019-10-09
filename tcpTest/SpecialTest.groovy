04f0f052423b2ddc2dbaff6702e44f0887040545
David Gutheinz <djgutheinz@outlook.com>
2019-10-09 17:50:29 -0500

    Add files via upload

---
+++ tcpTest/SpecialTest.groovy
@@ -0,0 +1,163 @@
+/*
+Test HS-210 Driver
+*/
+	def driverVer() { return "4.5.10" }
+
+metadata {
+	definition (name: "TP-Link HS210 Test}",
+    			namespace: "davegut",
+                author: "Dave Gutheinz",
+				importUrl: "https://raw.githubusercontent.com/DaveGut/Hubitat-TP-Link-Integration/master/DeviceDrivers/TP-LinkPlug-Switch(Hubitat).groovy"
+			   ) {
+		capability "Switch"
+        capability "Actuator"
+		capability "Refresh"
+		attribute "commsError", "bool"
+	}
+	preferences {
+		input ("refresh_Rate", "enum", title: "Device Refresh Interval (minutes)", 
+			   options: ["1", "5", "15", "30"], defaultValue: "30")
+		input ("shortPoll", "number",title: "Fast Polling Interval ('0' = disabled)",
+			   defaultValue: 0)
+		input ("debug", "bool", title: "Enable debug logging", defaultValue: false)
+		input ("descriptionText", "bool", title: "Enable description text logging", defaultValue: true)
+	}
+}
+def installed() {
+	log.info "Installing .."
+	runIn(2, updated)
+}
+def updated() {
+	log.info "Updating .."
+	unschedule()
+	state.errorCount = 0
+	updateDataValue("driverVersion", driverVer())
+	switch(refresh_Rate) {
+		case "1" : runEvery1Minute(refresh); break
+		case "5" : runEvery5Minutes(refresh); break
+		case "15" : runEvery15Minutes(refresh); break
+		default: runEvery30Minutes(refresh)
+	}
+	if (shortPoll == null) { device.updateSetting("shortPoll",[type:"number", value:0]) }
+	if (nameSync == "device" || nameSync == "hub") { runIn(5, syncName) }
+	runIn(5, refresh)
+}
+
+def on() {
+	logDebug("on")
+	sendCmd("""{"system":{"set_relay_state":{"state":1}}""", "commandResponse")
+}
+def off() {
+	logDebug("off")
+	sendCmd("""{"system":{"set_relay_state":{"state":0}}""", "commandResponse")
+}
+def commandResponse() { refresh() }
+def refresh() {
+	logDebug("refresh")
+	sendCmd("""{"system":{"get_sysinfo":{}}}""", "refreshResponse")
+}
+def refreshResponse(response) {
+	def cmdResponse = parseInput(response)
+	logDebug("refreshResponse: status = ${cmdResponse}")
+	def status = cmdResponse.system.get_sysinfo
+	def relayState = status.relay_state
+	if (getDataValue("plugNo")) {
+		status = status.children.find { it.id == getDataValue("plugNo") }
+		relayState = status.state
+	}
+	def pwrState = "off"
+	if (relayState == 1) { pwrState = "on"}
+	sendEvent(name: "switch", value: "${pwrState}")
+	if (type() == "Dimming Switch") {
+		sendEvent(name: "level", value: status.brightness)
+	}
+	logInfo("Switch: ${pwrState}")
+
+	if (shortPoll.toInteger() > 0) { runIn(shortPoll.toInteger(), refresh) }
+}
+
+private sendCmd(command, action) {
+	logDebug("sendCmd: command = ${command} // device IP = ${getDataValue("deviceIP")}, action = ${action}")
+	state.lastCommand = [command: "${command}", action: "${action}"]
+	runIn(5, setCommsError)
+	def myHubAction = new hubitat.device.HubAction(
+		outputXOR(command),
+		hubitat.device.Protocol.LAN,
+		[type: hubitat.device.HubAction.Type.LAN_TYPE_UDPCLIENT,
+		 destinationAddress: "${getDataValue("deviceIP")}:9999",
+		 encoding: hubitat.device.HubAction.Encoding.HEX_STRING,
+		 timeout: 5,
+		 callback: action])
+	sendHubCommand(myHubAction)
+}
+def parseInput(response) {
+	unschedule(setCommsError)
+	state.errorCount = 0
+	sendEvent(name: "commsError", value: false)
+	try {
+		def encrResponse = parseLanMessage(response).payload
+		def cmdResponse = parseJson(inputXOR(encrResponse))
+		return cmdResponse
+	} catch (error) {
+		logWarn "CommsError: Fragmented message returned from device."
+	}
+}
+def setCommsError() {
+	logDebug("setCommsError")
+	if (state.errorCount < 5) {
+		state.errorCount+= 1
+		repeatCommand()
+		logWarn("Attempt ${state.errorCount} to recover communications")
+	} else if (state.errorCount == 5) {
+		state.errorCount += 1
+		//	If a child device, update IPs automatically using the application.
+		if (getDataValue("applicationVersion")) {
+			logWarn("setCommsError: Parent commanded to poll for devices to correct error.")
+			parent.updateDevices()
+			runIn(90, repeatCommand)
+		}
+	} else {
+		sendEvent(name: "commsError", value: true)
+		logWarn "setCommsError: No response from device.  Refresh.  If off line " +
+				"persists, check IP address of device."
+	}
+}
+def repeatCommand() { 
+	logDebug("repeatCommand: ${state.lastCommand}")
+	sendCmd(state.lastCommand.command, state.lastCommand.action)
+}
+
+private outputXOR(command) {
+	def str = ""
+	def encrCmd = ""
+ 	def key = 0xAB
+	for (int i = 0; i < command.length(); i++) {
+		str = (command.charAt(i) as byte) ^ key
+		key = str
+		encrCmd += Integer.toHexString(str)
+	}
+   	return encrCmd
+}
+private inputXOR(encrResponse) {
+	String[] strBytes = encrResponse.split("(?<=\\G.{2})")
+	def cmdResponse = ""
+	def key = 0xAB
+	def nextKey
+	byte[] XORtemp
+	for(int i = 0; i < strBytes.length; i++) {
+		nextKey = (byte)Integer.parseInt(strBytes[i], 16)	// could be negative
+		XORtemp = nextKey ^ key
+		key = nextKey
+		cmdResponse += new String(XORtemp)
+	}
+	return cmdResponse
+}
+def logInfo(msg) {
+	if (descriptionText == true) { log.info "<b>${device.label} ${driverVer()}</b> ${msg}" }
+}
+def logDebug(msg){
+	if(debug == true) { log.debug "<b>${device.label} ${driverVer()}</b> ${msg}" }
+}
+def logWarn(msg){ log.warn "<b>${device.label} ${driverVer()}</b> ${msg}" }
+
+//	end-of-file
