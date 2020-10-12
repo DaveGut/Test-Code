5d4effab0d3e5f3b2998824324a0407218339161
David Gutheinz <djgutheinz@outlook.com>
2020-10-12 09:20:09 -0500

    Add files via upload

--- Test Code.groovy
+++ Test Code.groovy
@@ -1,5 +1,5 @@
 /*
-KEY_VOLUP; KEY_VOLDOWN; KEY_MUTE; KEY_SOURCE; KEY_TOOLS; KEY_MENU
+Test only.  Light the programming fire for WS Samsung Remote.
 */
 import groovy.json.JsonOutput
 metadata {
@@ -11,76 +11,70 @@ metadata {
 		command "sendKey", ["string"]
 		command "getData"
 		command "getToken"
+		command "volumeUp"
+		command "volumeDown"
+		command "mute"
+		command "source"
+		command "tools"
+		command "menu"
 	}
 	preferences {
 		input ("deviceIp", "text", title: "Device Ip")
-		input ("devicePort", "text", title: "Capability Port", defaultValue: "8001")
 	}
 }
-def installed() { updated() }
-
-def updated() {
-	updateDataValue("token", "12345678") 
-}
-
+def installed() { }
 def getData() {
-	//	Gets the name and uri, update device data.
-	def uri = "http://${deviceIp}:8001/api/v2/"
-	httpGet([uri: uri, timeout: 5]) { resp ->
-		logTrace("getData: name = ${resp.data.name} // uri ${resp.data.uri}")
-		updateDataValue("name", resp.data.name)
+	httpGet([uri: "http://${deviceIp}:8001/api/v2/", timeout: 5]) { resp ->
+		logTrace("getData: name = ${resp.data.name}")
 		updateDataValue("name64", resp.data.name.encodeAsBase64().toString())
 	}
 }
-
 def getToken() {
-	logTrace("getToken")
-	//	Gets the token and sets.
 	def name = getDataValue("name64")
-	def uri = "http://${deviceIp}:${devicePort}/api/v2/channels/samsung.remote.control?name=${name}"
+	logTrace("getToken: name = ${name}")
+	def uri = "https://${deviceIp}:8002/api/v2/channels/samsung.remote.control?name=${name}"
 	webSocketConnect(uri)
 	runIn(5, close)
 }
 
+def volumeUp() { sendKey("KEY_VOLUP") }
+def volumeDown() { sendKey("KEY_VOLDOWN") }
+def mute() { sendKey("KEY_MUTE") }
+def source() { sendKey("KEY_SOURCE") }
+def tools() { sendKey("KEY_TOOLS") }
+def menu() { sendKey("KEY_MENU") }
+
 def sendKey(String keyPress) {
-	log.trace "test: $keyPress"
-	def command = [
-		method: "ms.remote.control",
-		params: [
-			Cmd: "Click",
-			DataOfCmd: keyPress,
-			Option: "false",
-			TypeOfRemote: "SendRemoteKey"
-			]
-		]
+	def command = [method: "ms.remote.control", params: [
+		Cmd: "Click", DataOfCmd: keyPress, Option: "false", TypeOfRemote: "SendRemoteKey"]]
 	command = JsonOutput.toJson(command)
-	def cmdUri = getDataValue("cmdUri")
 	def token = getDataValue("token")
-	def uri = "http://${deviceIp}:${devicePort}/api/v2/channels/samsung.remote.control?token=${token}"
-	webSocketConnect(url)
+//	def name = getDataValue("name64")
+	def name = "LInux UPnP/1.0 Hubitat"
+	name = name.encodeAsBase64().toString()
+	logTrace "sendKey: key = ${keyPress}, token = ${token}"
+	def uri = "https://${deviceIp}:8002/api/v2/channels/samsung.remote.control?token=${token}"
+//	def uri = "https://${deviceIp}:8002/api/v2/channels/samsung.remote.control?name=${name}&token=${token}"
+	webSocketConnect(uri)
 	pauseExecution(200)
 	sendMessage(command)
 	runIn(3, close)
 }
 
 def webSocketConnect(String url) { interfaces.webSocket.connect(url) }
-
 def sendMessage(String message) { interfaces.webSocket.sendMessage(message) }
-
 def close() { interfaces.webSocket.close() }
-
-def webSocketStatus(message) { log.debug "webSocketStatus: $message" }
-
+def webSocketStatus(message) { logInfo "webSocketStatus: $message" }
 def parse(message) {
 	def respData = parseJson(message)
 	if (respData.event == "ms.channel.connect" && respData.token) { 
-		log.trace respData.token
+		logTrace("parse: token = ${respData.token}")
 		updateDataValue("token", respData.token)
 	} else {
 		logInfo("parse: ${message}")
 	}
 }
 
-def logTrace(msg) { log.trace "${msg}" }
-
-def logInfo(msg) { log.info "${msg}" }
+def logTrace(msg) { log.trace "TV Test || ${msg}" }
+def logInfo(msg) { log.info "TV Test || ${msg}" }
+def logDebug(msg) { log.debug "TV Test || ${msg}" }
