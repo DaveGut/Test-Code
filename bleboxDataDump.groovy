bd7434de0462ca69c761454d5ed45ae6cb831a14
David Gutheinz <djgutheinz@outlook.com>
2021-07-08 08:40:39 -0500

    Add files via upload

---
+++ bleboxDataDump.groovy
@@ -0,0 +1,73 @@
+/*
+blebox tempSensorPro information driver.
+*/
+//	===== Definitions, Installation and Updates =====
+metadata {
+	definition (name: "blebox data dump",
+				namespace: "davegut",
+				author: "Dave Gutheinz",
+				importUrl: "https://raw.githubusercontent.com/DaveGut/HubitatActive/master/bleBoxDevices/Drivers/tempSensor.groovy"
+			   ) {
+		capability "Refresh"
+	}
+	preferences {
+		input ("device_IP", "text", title: "Device IP")
+	}
+}
+
+def installed() {
+	runIn(2, updated)
+}
+
+def updated() {
+	if (!device_IP) {
+		logWarn("updated:  device_IP  is not set.")
+		return
+	}
+
+	refresh()
+}
+
+def refresh() {
+	logInfo("STARTING SEQUENCE")
+	logInfo("INFO")
+	sendGetCmd("/info", "commandParse")
+	pauseExecution(2000)
+	logInfo("STATE")
+	sendGetCmd("/state", "commandParse")
+	pauseExecution(2000)
+	logInfo("SETTINGS")
+	sendGetCmd("/api/settings/state", "commandParse")
+}
+
+def commandParse(response) {
+	def cmdResponse = parseInput(response)
+	logDebug("commandParse: cmdResponse = ${cmdResponse}")
+}
+
+//	===== Communications =====
+private sendGetCmd(command, action){
+	logDebug("sendGetCmd: ${command} / ${action} / ${device_IP}")
+	sendHubCommand(new hubitat.device.HubAction("GET ${command} HTTP/1.1\r\nHost: ${device_IP}\r\n\r\n",
+				   hubitat.device.Protocol.LAN, null,[callback: action]))
+}
+
+def parseInput(response) {
+	unschedule(setCommsError)
+	state.errorCount = 0
+	try {
+		def jsonSlurper = new groovy.json.JsonSlurper()
+		return jsonSlurper.parseText(response.body)
+	} catch (error) {
+		logWarn "CommsError: ${error}."
+	}
+}
+
+//	===== Utility Methods =====
+def logInfo(msg) { log.info "${msg}" }
+
+def logDebug(msg){ log.debug "${msg}" }
+
+def logWarn(msg){ log.warn "${msg}" }
+
+//	end-of-file
